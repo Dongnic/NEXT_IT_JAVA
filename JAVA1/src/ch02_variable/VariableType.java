dd4e8b653fc611715a23969abcddd0d5694bd2fe
Dongnic <alfothsus3@gmail.com>
2022-04-12 11:58:32 +0900

    2022.04.12(화) Class02

---
+++ JAVA1/src/ch02_variable/VariableType.java
@@ -0,0 +1,26 @@
+package ch02_variable;
+
+public class VariableType {
+	// 변수의 타입(기본타입)
+	
+	// 정수 타입 
+	byte byteVar = 1;
+	short shorVar = 2;
+	int intVar = 3; 
+	long longVar = 4;
+	
+	char charVar = 5;
+	char charVal = '짱';
+	
+	// 실수 타입
+	double doubleVar = 3.14;
+	float floatVar = 3.14f;
+	float floatVal = 3.14F;
+	long longVar1 = 4l;
+	long longVal1 = 4L;
+	
+	// 불리언 타입 (True/False)
+	boolean boolVar = true;
+	boolean boolVal = false;
+	
+}
